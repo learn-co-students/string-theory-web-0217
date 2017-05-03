b42ef2609a79269805f7a5ddd7c019e7ae8c41ee
github username <github email address>
2017-05-02 21:04:13 -0400

    Done.

--- lib/string_theory.rb
+++ lib/string_theory.rb
@@ -1,3 +1,14 @@
 def sort_by_appearance(characters_to_sort, long_text)
-  # code goes here
+  searched_chars = characters_to_sort.split("")
+  long_text_to_search = long_text.split("")
+  char_count = {}
+  searched_chars.each do |char|
+    char_count[char] = 0
+  end
+  long_text_to_search.each do |char|
+    if char_count[char]
+      char_count[char] += 1
+    end
+  end
+  char_count.sort_by{ |_k, v| v }.map {|tuple| tuple[0]}.join().reverse
 end
